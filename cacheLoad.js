6c498801f5f34c1485749baaa83213b17783cec0
nicolas4132 <nicolas.meyer@bbzbl-it>
2022-03-30 13:24:16 +0200

    Klassenauswahl
    erstellen und befüllen der Klassenauswahl-Dropdown funktion

--- cacheLoad.js
+++ cacheLoad.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
     }).done(function(data) {
         $('#jobSelector').append('<option>Ihre Auswahl ... </option>');
         $.each(data, function(key, value) {
-            console.log(value)
             if (value.beruf_id == localStorage.getItem('beruf_id')) {
                 $('#jobSelector').append('<option value=' + value.beruf_id + 'selected>' + value.beruf_name + '</option>')
             } else {
@@ -30,5 +29,35 @@ $(document).ready(function() {
         localStorage.setItem('beruf_id', this.value);
         apiClassCall(this.value);
     })
+
+    function apiClassCall(beruf_id) {
+        console.log(beruf_id)
+        $.ajax({
+            type: "GET",
+            url: "http://sandbox.gibm.ch/klassen.php?beruf_id=" + beruf_id,
+            data: { format: 'JSON' },
+            dataType: 'json'
+        }).done(function(data) {
+            $.each(data, function(key, value) {
+                console.log(value)
+                if (value.klasse_id == localStorage.getItem('klasse_id')) {
+                    $('#classSelector').append('<option value=' + value.klasse_id + 'selected>' + value.klasse_longname + '</option>')
+                } else {
+                    $('#classSelector').append('<option value=' + value.klasse_id + '>' + value.klasse_longname + '</option>')
+                }
+            })
+        }).fail(function() {
+            $('#errorMessage').text("Fehler aufgetreten");
+        })
+    }
+
+    $('#classSelector').change(function(e) {
+        localStorage.setItem('klasse_id', this.value);
+        tableFill(this.value);
+    })
+
+    function tableFill(klasse_id) {
+
+    }
 
 })
